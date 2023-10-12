f13228935054a63eed694a65eb76e1fcd264642e
taufiksty <taufik.satya09@gmail.com>
2023-10-12 15:53:02 +0700

    [feat] (exam-page): Add exam page

---
+++ exam-page/script.js
@@ -0,0 +1,147 @@
+/**
+ * Reponsive Navbar
+ */
+const menuMobileOpen = document.getElementById('menu-mobile-open');
+const menuMobileClose = document.getElementById('menu-mobile-close');
+const menuMobile = document.getElementById('menu-mobile');
+
+menuMobileOpen.addEventListener('click', () => {
+	menuMobile.classList.toggle('menu-opened');
+});
+
+menuMobileClose.addEventListener('click', () => {
+	menuMobile.classList.toggle('menu-opened');
+});
+
+/**
+ * Fetch Data
+ */
+async function fetchData(url) {
+	try {
+		const response = await fetch(url);
+
+		if (!response.ok) {
+			throw new Error('Response error');
+		}
+
+		const resJson = await response.json();
+
+		return resJson.data;
+	} catch (error) {
+		console.error('Fetch error:', error);
+		throw error;
+	}
+}
+
+const examName = document.getElementById('exam-name');
+const numQuestion = document.getElementById('num-question');
+const questionNode = document.getElementById('question');
+const answers = document.getElementById('answers');
+const prevQuestion = document.getElementById('prev-question');
+const nextQuestion = document.getElementById('next-question');
+const listQuestion = document.getElementById('list-question');
+
+let currentQuestion = 'question-1';
+let currentAnswer = {};
+
+function handleAnswerClick(e) {
+	const num = e.target.getAttribute('data-num');
+	const ans = e.target.getAttribute('data-key');
+	currentAnswer = { ...currentAnswer, [num]: ans };
+	console.log(currentAnswer);
+	initAnswerControl();
+}
+
+async function updateQuestion() {
+	const data = await fetchData(
+		'https://9cac7863-db16-495f-9f50-d4a119739f66.mock.pstmn.io/exam-1?questions=true',
+	);
+
+	console.log(data);
+
+	examName.innerHTML = data.exam_name;
+
+	let listQuestionArr = [];
+
+	data.questions.forEach((question, i) => {
+		listQuestionArr.push(`<div
+						class="text-slate-700 md:text-[20px] p-2 md:p-3 hover:bg-zinc-100 text-center rounded-md border border-slate-300 cursor-pointer ${
+							currentAnswer.hasOwnProperty(i + 1) ? 'bg-[#5EEA6C]' : 'bg-white'
+						}">
+						${i + 1}
+					</div>`);
+
+		if (currentQuestion === question.question_id) {
+			numQuestion.innerHTML = i + 1;
+			questionNode.innerHTML = question.question;
+
+			let questionAnswersArr = [];
+			const questionAnswers = question.answers;
+
+			for (const key in questionAnswers) {
+				questionAnswersArr.push(`<div data-num="${
+					i + 1
+				}" data-key="${key}" onclick="handleAnswerClick"
+									class="ans text-[12px] text-slate-700 md:text-[20px] rounded-lg w-11/12 p-2 md:p-3 border border-[#D9D9D9] cursor-pointer">
+									${key}. ${questionAnswers[key]}
+								</div>`);
+			}
+			answers.innerHTML = questionAnswersArr.join('');
+		}
+	});
+
+	listQuestion.innerHTML = listQuestionArr.join('');
+
+	initAnswerControl();
+}
+
+function initAnswerControl() {
+	document.querySelectorAll('.ans').forEach((a) => {
+		a.addEventListener('click', handleAnswerClick);
+
+		a.addEventListener('mouseover', () => {
+			a.style.border = '1px solid #008D91';
+			a.style.color = '#008D91';
+		});
+
+		a.addEventListener('mouseout', () => {
+			a.style.border = '1px solid #D9D9D9';
+			a.style.color = '#111827';
+		});
+
+		a.style.backgroundColor = 'transparent';
+
+		if (
+			a.getAttribute('data-num') in currentAnswer &&
+			currentAnswer[a.getAttribute('data-num')] === a.getAttribute('data-key')
+		) {
+			a.style.backgroundColor = '#D9D9D9';
+		}
+	});
+}
+
+updateQuestion();
+
+prevQuestion.addEventListener('click', () => {
+	if (Number(currentQuestion.split('-')[1]) == 1) {
+		return;
+	}
+
+	numCurrQuestion = Number(currentQuestion.split('-')[1]);
+	currentQuestion = `question-${numCurrQuestion - 1}`;
+
+	console.log(currentQuestion);
+	updateQuestion();
+});
+
+nextQuestion.addEventListener('click', () => {
+	if (Number(currentQuestion.split('-')[1]) >= 10) {
+		return;
+	}
+
+	numCurrQuestion = Number(currentQuestion.split('-')[1]);
+	currentQuestion = `question-${numCurrQuestion + 1}`;
+
+	console.log(currentQuestion);
+	updateQuestion();
+});
