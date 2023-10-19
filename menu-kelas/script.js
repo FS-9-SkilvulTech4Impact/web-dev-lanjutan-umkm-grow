7a1c459fdefaa4fbe313b5e8fffb9ba75db28822
taufiksty <taufik.satya09@gmail.com>
2023-10-19 14:30:23 +0700

    [fix] (dev): Add navigation route detail kelas

--- menu-kelas/script.js
+++ menu-kelas/script.js
@@ -1,44 +1,44 @@
-const menuMobileOpen = document.getElementById("menu-mobile-open");
-const menuMobileClose = document.getElementById("menu-mobile-close");
-const menuMobile = document.getElementById("menu-mobile");
+const menuMobileOpen = document.getElementById('menu-mobile-open');
+const menuMobileClose = document.getElementById('menu-mobile-close');
+const menuMobile = document.getElementById('menu-mobile');
 
-menuMobileOpen.addEventListener("click", () => {
-  menuMobile.classList.toggle("menu-opened");
+menuMobileOpen.addEventListener('click', () => {
+	menuMobile.classList.toggle('menu-opened');
 });
 
-menuMobileClose.addEventListener("click", () => {
-  menuMobile.classList.toggle("menu-opened");
+menuMobileClose.addEventListener('click', () => {
+	menuMobile.classList.toggle('menu-opened');
 });
 
-const formEl = document.querySelector("form");
-const inputEl = document.getElementById("search-input");
-const menuKelas = document.getElementById("list-kelas");
+const formEl = document.querySelector('form');
+const inputEl = document.getElementById('search-input');
+const menuKelas = document.getElementById('list-kelas');
 
 async function searchKelas(input) {
-  inputData = inputEl.value;
-  const respons = await fetch(
-    "https://65276836917d673fd76da682.mockapi.io/menu-kelas"
-  );
-  let kelas = await respons.json();
+	inputData = inputEl.value;
+	const respons = await fetch(
+		'https://65276836917d673fd76da682.mockapi.io/menu-kelas',
+	);
+	let kelas = await respons.json();
 
-  console.log(inputData.length);
+	console.log(inputData.length);
 
-  if (inputData.length === 0) {
-    renderList(kelas);
-  } else {
-    kelas = kelas.filter((item) => {
-      return item.name.toLowerCase().includes(input);
-    });
-    renderList(kelas);
-  }
+	if (inputData.length === 0) {
+		renderList(kelas);
+	} else {
+		kelas = kelas.filter((item) => {
+			return item.name.toLowerCase().includes(input);
+		});
+		renderList(kelas);
+	}
 }
 
 function renderList(kelas) {
-  menuKelas.innerHTML = "";
-  kelas.map((item, index) => {
-    let listKelas = `
+	menuKelas.innerHTML = '';
+	kelas.map((item, index) => {
+		let listKelas = `
       <div class="md:mx-[154px] mb-8 hover:bg-slate-100 cursor-pointer sm:mx-[70px]"
-        <a href="#">
+        <a href="/detail-kelas">
           <div class="flex gap-5">
             <img src="${item.image}" alt="" class="w-52 rounded-md">
               <div>
@@ -52,17 +52,17 @@ function renderList(kelas) {
         <hr class="mt-4">
       </div>
           `;
-    menuKelas.innerHTML += listKelas;
-  });
+		menuKelas.innerHTML += listKelas;
+	});
 }
 
-formEl.addEventListener("input", async (event) => {
-  event.preventDefault();
+formEl.addEventListener('input', async (event) => {
+	event.preventDefault();
 
-  setTimeout(() => {
-    searchKelas(event.target.value);
-  }, 1000);
-  console.log();
+	setTimeout(() => {
+		searchKelas(event.target.value);
+	}, 1000);
+	console.log();
 });
 
 searchKelas();
