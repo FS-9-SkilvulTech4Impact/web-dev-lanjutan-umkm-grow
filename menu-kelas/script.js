0654c4a5f7c7e59caae6b1dd754212ba268e00ac
Rico Mardiansyah <ricomardiansyah27@gmail.com>
2023-10-19 11:11:52 +0700

    create search live

--- menu-kelas/script.js
+++ menu-kelas/script.js
@@ -10,39 +10,59 @@ menuMobileClose.addEventListener("click", () => {
   menuMobile.classList.toggle("menu-opened");
 });
 
-let menuKelas = document.getElementById("list-kelas");
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const menuKelas = document.getElementById("list-kelas");
 
-async function getListKelas() {
-  try {
-    let respons = await fetch(
-      "https://65276836917d673fd76da682.mockapi.io/menu-kelas"
-    );
-    let kelas = await respons.json();
+async function searchKelas(input) {
+  inputData = inputEl.value;
+  const respons = await fetch(
+    "https://65276836917d673fd76da682.mockapi.io/menu-kelas"
+  );
+  let kelas = await respons.json();
 
-    console.log(kelas);
+  console.log(inputData.length);
 
-    kelas.map((item, index) => {
-      let listKelas = `
-        <div class="mx-[154px] mb-8 hover:bg-slate-100 cursor-pointer"
-          <a href="#">
-            <div class="flex gap-5">
-              <img src="${item.image}" alt="" class="w-52 rounded-md">
-              <div>
-                <h4 class="font-semibold">${item.name}</h4>
-                <p class="text-[14px]">${item.detail_p}</p>
-                <p class="text-[14px]">${item.rating}</p>
-              </div>           
-              <h4 class="text-red-600 ml-auto">${item.ket}</h4>
-            </div>
-          </a>
-          <hr class="mt-4">
-        </div>  
-      `;
-      menuKelas.innerHTML += listKelas;
+  if (inputData.length === 0) {
+    renderList(kelas);
+  } else {
+    kelas = kelas.filter((item) => {
+      return item.name.toLowerCase().includes(input);
     });
-  } catch (error) {
-    console.log(error);
+    renderList(kelas);
   }
 }
 
-getListKelas();
+function renderList(kelas) {
+  menuKelas.innerHTML = "";
+  kelas.map((item, index) => {
+    let listKelas = `
+      <div class="md:mx-[154px] mb-8 hover:bg-slate-100 cursor-pointer sm:mx-[70px]"
+        <a href="#">
+          <div class="flex gap-5">
+            <img src="${item.image}" alt="" class="w-52 rounded-md">
+              <div>
+                <h4 class="font-semibold">${item.name}</h4>
+                <p class="text-[14px]">${item.detail_p}</p>
+              <p class="text-[14px]">${item.rating}</p>
+              </div>
+            <h4 class="text-red-600 ml-auto">${item.ket}</h4>
+          </div>
+        </a>
+        <hr class="mt-4">
+      </div>
+          `;
+    menuKelas.innerHTML += listKelas;
+  });
+}
+
+formEl.addEventListener("input", async (event) => {
+  event.preventDefault();
+
+  setTimeout(() => {
+    searchKelas(event.target.value);
+  }, 1000);
+  console.log();
+});
+
+searchKelas();
